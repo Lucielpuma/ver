cd1602fb9006dd0a90c4cf35d2ae0f97afe4bc6e
Lucielpuma <111878110+Lucielpuma@users.noreply.github.com>
2023-01-09 12:56:18 -0500

    Add files via upload

---
+++ unidad_3/1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    float b;
+
+    a=6;
+    b=5;
+
+    int suma = a + b;
+    int resta = a - b;
+    int mult = a * b;
+    float div = a / b;
+
+
+
+    std::cout <<"la suma es : "<<suma<< std::endl;
+    std::cout <<"la resta es: "<<resta<< std::endl;
+    std::cout <<"la mult es: "<<mult<< std::endl;
+    std::cout <<"la div es: "<<div<< std::endl;
+    
+    return 0;
+}
---
+++ unidad_3/ejemplo_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char val = 'c';
+
+    cout << sizeof (val) << endl;
+
+    int valor= static_cast<int>(val);
+    cout<< valor <<endl;
+    return 0;
+}
